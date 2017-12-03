ea04a64e3455281928f2ca2d27cc4b5d8cd3ab2a
lahigham <higham.laurel@gmail.com>
2017-12-03 17:28:34 -0500

    updated scale values, fixed big hecagon array

--- HexStateMachine/HexStateMachine.ino
+++ HexStateMachine/HexStateMachine.ino
@@ -45,7 +45,7 @@ uint32_t segment2[] = {7,8,9,10,11,12,13,28,29,30,31,32,33,34,72,73,74,75,76,77,
 uint32_t segment3[] = {14,15,16,17,18,19,20,35,36,37,38,39,40,41,42,43,44,45,46,47,60,61,62,63,64,65,103,104,105,106,107,88,89,90,91,92};
 
 //pixel index for each hexagon
-uint32_t bigHex[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41};
+uint32_t bigHex[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41};
 uint32_t middleHex[] = {42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77};
 uint32_t smallHex[]= {78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107};
 
@@ -80,13 +80,16 @@ void setup() {
 
   //LED strip setup
   strip.begin();
-  strip.setBrightness(190); //75% brightness
+  strip.setBrightness(50); //75% brightness
   strip.show();
 
   //color setup
   uint32_t color_seg1 = 0;
   uint32_t color_seg2 = 0;
   uint32_t color_seg3 = 0;
+
+  //state machine setup
+  state = 2;
 }
 
 void loop() {
@@ -104,7 +107,7 @@ void stateMachine(){
     break;
     case 1: //Color fade through whole strand mode
     Serial.println("State 1");
-    theaterChaseRainbow(60);
+    theaterChaseRainbow(40);
     break;
     case 2: //Flashy through each hex mode
     Serial.println("State 2");
@@ -135,6 +138,7 @@ void stateMachine(){
     case 3: //Clear lights
     Serial.println("State 3");
     strip.clear();
+    strip.show();
     break;
   }
 }
@@ -153,23 +157,24 @@ void changeState(){
 void softPotUpdate(){
   if (analogRead(topPot) != 0) {
     topVal = analogRead(topPot);
-    topVal = map(topVal, 0, 1023, 0, 35);
+    topVal = map(topVal, 24, 1010, 0, 36);
   }
   if (analogRead(middlePot) != 0) {
     middleVal = analogRead(middlePot);
-    middleVal = map(middleVal, 0, 1023, 0, 35);
+    middleVal = map(middleVal, 24, 1010, 0, 36);
   }
   if (analogRead(bottomPot) != 0) {
     bottomVal = analogRead(bottomPot);
-    bottomVal = map(bottomVal, 0, 1023, 0, 35);
+    bottomVal = map(bottomVal, 24, 1010, 0, 36);
   }
 }
 
 //read sound sensor, scale to 32-bit RGB color values
 void soundUpdate(){
   if (analogRead(soundPin) <= 50){
+      //Serial.println(analogRead(soundPin));
       amplitude = analogRead(soundPin);
-      amplitude = map(amplitude, 0, 28, 0, 255);
+      amplitude = map(amplitude, 6, 20, 10, 255);
   }
 }
 
@@ -184,20 +189,34 @@ void colorUpdate(){
 //change what lights are on based on soft pots, color based on sound
 void lightSensing(){
   //segment 1 update
-  for (uint16_t i = 0; i < topVal; i++){
-    Serial.print(i);
-    strip.setPixelColor(segment1[i], color_seg1);
+  for (uint16_t i = 0; i < sizeof(segment1)/sizeof(uint32_t); i++){
+    //Serial.print(i);
+    if (i < topVal){
+      strip.setPixelColor(segment1[i], color_seg1);
+    }
+    else{
+      strip.setPixelColor(segment1[i], off);
+    }
   }
   //segment 2 update
-  for (uint16_t i = 0; i < middleVal; i++){
-    Serial.print(i);
-    strip.setPixelColor(segment2[i], color_seg2);
+  for (uint16_t i = 0; i < sizeof(segment2)/sizeof(uint32_t); i++){
+    //Serial.print(i);
+    if (i < middleVal){
+      strip.setPixelColor(segment2[i], color_seg2);
+    }
+    else{
+      strip.setPixelColor(segment2[i], off);
+    }
   }  
-  
   //segment 3 update
-  for (uint16_t i = 0; i < bottomVal; i++){
-    Serial.print(i);
-    strip.setPixelColor(segment3[i], color_seg3);
+  for (uint16_t i = 0; i < sizeof(segment3)/sizeof(uint32_t); i++){
+    //Serial.print(i);
+    if (i < bottomVal){
+      strip.setPixelColor(segment3[i], color_seg3);
+    }
+    else{
+      strip.setPixelColor(segment3[i], off);
+    }
   }
   strip.show();
   delay(100);
